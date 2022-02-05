ab82a4a1f64b1359d64a170efd392b1efe5534eb
qayumi <elyasqayumi@gmail.com>
2022-02-05 17:44:44 +0300

    update

---
+++ karimaFullRest/src/main/java/com/example/karimafullrest/KarimaFullRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.karimafullrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarimaFullRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarimaFullRestApplication.class, args);
+	}
+}
---
+++ karimaFullRest/src/test/java/com/example/karimafullrest/KarimaFullRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karimafullrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarimaFullRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
